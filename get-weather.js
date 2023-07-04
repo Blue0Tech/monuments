82c13866ce90592098508681d155fe9575eb03ef
Pruthvi Shrikaanth <pruthvishrikaanth@gmail.com>
2023-07-04 19:47:36 +0100

    Added weather prediction functionality

--- get-weather.js
+++ get-weather.js
@@ -1,6 +1,7 @@
 let coords = {};
 $(document).ready(function() {
     getCoords();
+    getWeather();
 });
 function getCoords() {
     let searchParams = new URLSearchParams(window.location.search);
@@ -13,3 +14,24 @@ function getCoords() {
         window.history.back();
     };
 };
+function getWeather() {
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/weather?lat=${coords.dest_lat}&lon=${coords.dest_long}&appid=eb1dea078d579cb3867d8d8bd5bef15c`,
+        type: 'get',
+        success: function(response) {
+            let name = response.name;
+            let weather = response.weather[0].main;
+            $('scene_container').append(`
+                <a-entity gps-entity-place="latitude: ${coords.dest_lat}; longitude: ${coords.dest_long};">
+                    <a-entity>
+                        <a-text height="50" value="The weather is ${weather} at ${name}."></a-text>
+                    </a-entity>
+                </a-entity>
+            `);
+        },
+        error: function(error) {
+            console.log(error.responseJSON.cod);
+            console.log(error.responseJSON.message);
+        }
+    });
+}
